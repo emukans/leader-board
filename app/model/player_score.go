59ffc600983e4ca4a41a19d8ad29ba360e897a9f
emukans <mukans.work@gmail.com>
2020-09-27 11:35:03 +0300

    Move utility functions out of model repositories

--- app/model/player_score.go
+++ app/model/player_score.go
@@ -139,39 +139,6 @@ func FindScoresAroundName(name string, pageLimit int) ([]PlayerScore, error)  {
 	return result, nil
 }
 
-func calculateScoreRange(below int, above int, total int) (int, int) {
-	var lowerBound int
-	var upperBound int
-
-	limit := (total - 1) / 2
-
-	if below > limit && above > limit {
-		lowerBound = limit
-		upperBound = total - limit - 1
-	} else if below <= limit && above <= limit {
-		lowerBound = below
-		upperBound = above
-	} else if below <= limit && above >= limit {
-		lowerBound = below
-		upperBound = total - 1 - below
-	} else if below >= limit && above <= limit {
-		upperBound = above
-		lowerBound = total - 1 - above
-	}
-
-	return lowerBound, upperBound
-}
-
-func buildScoresFromSQLResult(rowList *sql.Rows) []PlayerScore {
-	var result []PlayerScore
-	for rowList.Next() {
-		var score PlayerScore
-		rowList.Scan(&score.Id, &score.Name, &score.Score, &score.UpdatedAt, &score.CreatedAt)
-		result = append(result, score)
-	}
-	return result
-}
-
 func FindScoreCount() (int, error) {
 	db, err := GetDBConnection()
 	if err != nil {
